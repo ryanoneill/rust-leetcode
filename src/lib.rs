f703b90a77c97441ac97f997b239e575a45b76da
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-22 08:56:13 -0700

    Added rudimentary Stack implementation

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 
 pub mod list_node;
 pub mod list_node_additions;
+pub mod stack;
 
 pub mod two_sum; // 1
 pub mod add_two_numbers; // 2
@@ -11,6 +12,7 @@ pub mod median_of_two_sorted_arrays; // 4
 pub mod longest_palindromic_substring; // 5
 
 pub mod remove_nth_node_from_end_of_list; // 19
+pub mod valid_parentheses; // 20
 
 pub mod swap_nodes_in_pairs; // 24
 
---
+++ src/stack.rs
@@ -0,0 +1,98 @@
+pub struct Stack<T> {
+    items: Vec<T>,
+}
+
+impl<T> Stack<T> {
+
+    pub fn new() -> Self {
+        Stack { items: vec![] }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        if self.is_empty() { None }
+        else { self.items.get(self.len() - 1) }
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+    pub fn push(&mut self, elem: T) {
+        self.items.push(elem);
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Stack;
+
+    #[test]
+    fn empty_when_new() {
+        let stack: Stack<i32> = Stack::new();
+        assert!(stack.is_empty());
+        assert_eq!(stack.len(), 0);
+    }
+
+    #[test]
+    fn not_empty_when_added_to() {
+        let mut stack = Stack::new();
+        stack.push(1);
+        assert!(!stack.is_empty());
+        assert_eq!(stack.len(), 1);
+    }
+
+    #[test]
+    fn multiple_elements_can_be_added() {
+        let mut stack = Stack::new();
+        stack.push('a');
+        stack.push('b');
+        stack.push('c');
+        stack.push('d');
+        stack.push('e');
+        assert!(!stack.is_empty());
+        assert_eq!(stack.len(), 5);
+    }
+
+    #[test]
+    fn peek_on_empty_returns_none() {
+        let stack: Stack<i32> = Stack::new();
+        let value = stack.peek();
+        assert_eq!(value, None);
+    }
+
+    #[test]
+    fn peek_retrieves_the_last_element() {
+        let mut stack = Stack::new();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        let value = stack.peek();
+        assert_eq!(value, Some(&3));
+    }
+
+    #[test]
+    fn pop_on_empty_returns_none() {
+        let mut stack: Stack<String> = Stack::new();
+        let value = stack.pop();
+        assert_eq!(value, None);
+    }
+
+    #[test]
+    fn pop_on_non_empty_returns_owned_value() {
+        let mut stack: Stack<String> = Stack::new();
+        stack.push("hello".to_string());
+        stack.push("goodbye".to_string());
+        let value = stack.pop();
+        assert!(value.is_some());
+        assert_eq!(value.unwrap(), "goodbye");
+    }
+
+}
