bca021febbbcfd17cd1d6002557957fe59fd88a0
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-22 09:39:29 -0700

    Added Merge Two Sorted Lists

--- src/merge_two_sorted_lists.rs
+++ src/merge_two_sorted_lists.rs
@@ -5,12 +5,59 @@ pub struct Solution;
 
 impl Solution {
 
-    // TODO: Implement
     pub fn merge_two_lists(
-        _list1: Option<Box<ListNode>>,
-        _list2: Option<Box<ListNode>>
+        list1: Option<Box<ListNode>>,
+        list2: Option<Box<ListNode>>
     ) -> Option<Box<ListNode>> {
-        None
+        if list1.is_empty() { list2 }
+        else if list2.is_empty() { list1 }
+        else {
+            let mut list1 = list1;
+            let mut list2 = list2;
+            let mut result = None;
+            let mut current = &mut result;
+            let mut first = true;
+
+            while !list1.is_empty() || !list2.is_empty() {
+                if first {
+                    let list1_value = list1.get_value();
+                    let list2_value = list2.get_value();
+
+                    if list1_value <= list2_value {
+                        result = list1;
+                        current = &mut result;
+                        list1 = current.take_next();
+                    } else {
+                        result = list2;
+                        current = &mut result;
+                        list2 = current.take_next();
+                    }
+
+                    first = false;
+                } else if list1.is_empty() {
+                    current.set_next(list2);
+                    list2 = None;
+                } else if list2.is_empty() {
+                    current.set_next(list1);
+                    list1 = None;
+                } else {
+                    let list1_value = list1.get_value();
+                    let list2_value = list2.get_value();
+
+                    if list1_value <= list2_value {
+                        current.set_next(list1);
+                        current = current.refer_next();
+                        list1 = current.take_next();
+                    } else {
+                        current.set_next(list2);
+                        current = current.refer_next();
+                        list2 = current.take_next();
+                    }
+                }
+            }
+
+            result
+        }
     }
 
 }
@@ -20,7 +67,6 @@ mod tests {
     use crate::list_node::ListNode;
     use super::Solution;
 
-    #[ignore]
     #[test]
     fn example_1() {
         let items1 = vec![1, 2, 4];
@@ -31,7 +77,6 @@ mod tests {
         assert_eq!(ListNode::to_vec(result), vec![1,1,2,3,4,4]);
     }
 
-    #[ignore]
     #[test]
     fn example_2() {
         let items1 = vec![];
@@ -42,7 +87,6 @@ mod tests {
         assert_eq!(ListNode::to_vec(result), vec![]);
     }
 
-    #[ignore]
     #[test]
     fn example_3() {
         let items1 = vec![];
