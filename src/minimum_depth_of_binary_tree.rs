36322e597ed3a0206b15a0f7386007c5d7b07fcb
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-24 11:11:39 -0700

    Use Codec in Minimum Depth of Binary Tree

--- src/minimum_depth_of_binary_tree.rs
+++ src/minimum_depth_of_binary_tree.rs
@@ -21,6 +21,7 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
+    use crate::serialize_and_deserialize_binary_tree::Codec;
     use crate::tree_node::TreeNode;
     use crate::tree_node_additions::TreeNodeAdditions;
     use std::cell::RefCell;
@@ -29,25 +30,19 @@ mod tests {
 
     #[test]
     fn example_1() {
-        // let items = vec![Some(3), Some(9), Some(20), None, None, Some(15), Some(7)];
-        let seven = TreeNodeAdditions::new(7);
-        let fifteen = TreeNodeAdditions::new(15);
-        let twenty = TreeNodeAdditions::with_children(20, fifteen, seven);
-        let nine = TreeNodeAdditions::new(9);
-        let three = TreeNodeAdditions::with_children(3, nine, twenty);
-        let result = Solution::min_depth(three);
+        let data = "[3,9,20,null,null,15,7]".to_string();
+        let codec = Codec::new();
+        let root = codec.deserialize(data);
+        let result = Solution::min_depth(root);
         assert_eq!(result, 2);
     }
 
     #[test]
     fn example_2() {
-        // let items = vec![Some(2), None, Some(3), None, Some(4), None, Some(5), None, Some(6)];
-        let six = TreeNodeAdditions::new(6);
-        let five = TreeNodeAdditions::with_children(5, None, six);
-        let four = TreeNodeAdditions::with_children(4, None, five);
-        let three = TreeNodeAdditions::with_children(3, None, four);
-        let two = TreeNodeAdditions::with_children(2, None, three);
-        let result = Solution::min_depth(two);
+        let data = "[2,null,3,null,4,null,5,null,6]".to_string();
+        let codec = Codec::new();
+        let root = codec.deserialize(data);
+        let result = Solution::min_depth(root);
         assert_eq!(result, 5);
     }
 
--- src/serialize_and_deserialize_binary_tree.rs
+++ src/serialize_and_deserialize_binary_tree.rs
@@ -18,11 +18,11 @@ use std::rc::Rc;
 /// Clarification: The input/output format is the same as how LeetCode
 /// serializes a binary tree. You do not necessarily need to follow this format
 /// so please be creative and come up with different approaches yourself.
-struct Codec;
+pub struct Codec;
 
 impl Codec {
 
-    fn new() -> Self {
+    pub fn new() -> Self {
         Codec { }
     }
 
@@ -158,6 +158,36 @@ mod tests {
         let two = TreeNodeAdditions::new(2);
         let one = TreeNodeAdditions::with_children(1, two, three);
         assert_eq!(result, one);
+    }
+
+    #[test]
+    fn real_example_1() {
+        let data = "[3,9,20,null,null,15,7]".to_string();
+        // let items = vec![Some(3), Some(9), Some(20), None, None, Some(15), Some(7)];
+        let seven = TreeNodeAdditions::new(7);
+        let fifteen = TreeNodeAdditions::new(15);
+        let twenty = TreeNodeAdditions::with_children(20, fifteen, seven);
+        let nine = TreeNodeAdditions::new(9);
+        let three = TreeNodeAdditions::with_children(3, nine, twenty);
+
+        let codec = Codec::new();
+        let result = codec.deserialize(data);
+        assert_eq!(result, three);
+    }
+
+    #[test]
+    fn real_example_2() {
+        let data = "[2,null,3,null,4,null,5,null,6]".to_string();
+        // let items = vec![Some(2), None, Some(3), None, Some(4), None, Some(5), None, Some(6)];
+        let six = TreeNodeAdditions::new(6);
+        let five = TreeNodeAdditions::with_children(5, None, six);
+        let four = TreeNodeAdditions::with_children(4, None, five);
+        let three = TreeNodeAdditions::with_children(3, None, four);
+        let two = TreeNodeAdditions::with_children(2, None, three);
+
+        let codec = Codec::new();
+        let result = codec.deserialize(data);
+        assert_eq!(result, two);
     }
 
 }
