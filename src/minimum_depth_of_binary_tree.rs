a23c44b3e16693af6ae8b8bd4ca3553b313869b8
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-24 08:32:08 -0700

    Added Minimum Depth of Binary Tree

--- src/minimum_depth_of_binary_tree.rs
+++ src/minimum_depth_of_binary_tree.rs
@@ -13,26 +13,49 @@ struct Solution;
 
 impl Solution {
 
-    // TODO: Implement
-    pub fn min_depth(_root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        0
+    pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        root.min_depth() as i32
     }
 
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::tree_node::TreeNode;
+    use crate::tree_node_additions::TreeNodeAdditions;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use super::Solution;
 
-    #[ignore]
     #[test]
     fn example_1() {
-        // TODO: Implement
+        // let items = vec![Some(3), Some(9), Some(20), None, None, Some(15), Some(7)];
+        let seven = TreeNodeAdditions::new(7);
+        let fifteen = TreeNodeAdditions::new(15);
+        let twenty = TreeNodeAdditions::with_children(20, fifteen, seven);
+        let nine = TreeNodeAdditions::new(9);
+        let three = TreeNodeAdditions::with_children(3, nine, twenty);
+        let result = Solution::min_depth(three);
+        assert_eq!(result, 2);
     }
 
-    #[ignore]
     #[test]
     fn example_2() {
-        // TODO: Implement
+        // let items = vec![Some(2), None, Some(3), None, Some(4), None, Some(5), None, Some(6)];
+        let six = TreeNodeAdditions::new(6);
+        let five = TreeNodeAdditions::with_children(5, None, six);
+        let four = TreeNodeAdditions::with_children(4, None, five);
+        let three = TreeNodeAdditions::with_children(3, None, four);
+        let two = TreeNodeAdditions::with_children(2, None, three);
+        let result = Solution::min_depth(two);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn root_only() {
+        let root = TreeNodeAdditions::new(10);
+        let result = Solution::min_depth(root);
+        assert_eq!(result, 1);
     }
 
 }
--- src/tree_node_additions.rs
+++ src/tree_node_additions.rs
@@ -1,6 +1,7 @@
 use crate::tree_node::TreeNode;
 use std::cell::RefCell;
 use std::cmp::max;
+use std::cmp::min;
 use std::collections::VecDeque;
 use std::rc::Rc;
 
@@ -13,6 +14,7 @@ pub trait TreeNodeAdditions {
     fn get_value(&self) -> Option<i32>;
     fn is_leaf(&self) -> bool;
     fn max_depth(&self) -> usize;
+    fn min_depth(&self) -> usize;
     fn set_left(&mut self, value: Self);
     fn set_right(&mut self, value: Self);
 
@@ -58,6 +60,22 @@ impl TreeNodeAdditions for Option<Rc<RefCell<TreeNode>>> {
                 let right = node.right.max_depth();
 
                 1 + max(left, right)
+            }
+        }
+    }
+
+    fn min_depth(&self) -> usize {
+        if self.is_none() { 0 }
+        else if self.is_leaf() { 1 }
+        else {
+            let rc = self.as_ref().unwrap();
+            let node = rc.borrow();
+            if node.left.is_some() && node.right.is_some() {
+                1 + min(node.left.min_depth(), node.right.min_depth())
+            } else if node.left.is_some() {
+                1 + node.left.min_depth()
+            } else {
+                1 + node.right.min_depth()
             }
         }
     }
