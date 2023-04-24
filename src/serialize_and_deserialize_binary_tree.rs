d21e7fd3c1ed87593d20dfefe5274eef7c423018
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-24 10:56:33 -0700

    Added Serialize and Deserialize Binary Tree (Still Needs More Testing)

--- src/serialize_and_deserialize_binary_tree.rs
+++ src/serialize_and_deserialize_binary_tree.rs
@@ -1,6 +1,8 @@
 use crate::tree_node::TreeNode;
 use crate::tree_node_additions::TreeNodeAdditions;
+use crate::vec_additions::VecAdditions;
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 /// Serialization is the process of converting a data structure or object into
@@ -24,69 +26,139 @@ impl Codec {
         Codec { }
     }
 
-    fn serialize(&self, _root: Option<Rc<RefCell<TreeNode>>>) -> String {
-        "".to_string()
+    pub fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
+        if root.is_none() { "[]".to_string() }
+        else {
+            let mut queue = VecDeque::new();
+            let mut items = Vec::new();
+
+            queue.push_back(root.clone());
+            while !queue.is_empty() {
+                let item = queue.pop_front().unwrap();
+                let value = item.get_value();
+                let value = value.map(|v| v.to_string()).unwrap_or("null".to_string());
+                items.push(value);
+
+                if item.is_some() {
+                    let rc = item.unwrap();
+                    let node = rc.borrow();
+                    let left = node.left.clone();
+                    let right = node.right.clone();
+                    queue.push_back(left);
+                    queue.push_back(right);
+                }
+            }
+            // Remove unneeded "null"s at end
+            items.pop_while(|i| i == "null");
+
+            // Concatenate into comma separated list with brackets.
+            let mut result = "[".to_string();
+            result += &items.join(",");
+            result += "]";
+
+            result
+        }
+
     }
 
-    fn deserialize(&self, _data: String) -> Option<Rc<RefCell<TreeNode>>> {
-        None
+    pub fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
+        if data == "[]" { None }
+        else {
+            let items = data.strip_prefix("[")
+                .and_then(|d| d.strip_suffix("]"))
+                .map(|d| d.split(',').collect())
+                .unwrap_or(vec![]);
+            let items: Vec<Option<i32>> = items.iter()
+                .map(|&v| v.parse().ok())
+                .collect();
+            let items: Vec<Option<Rc<RefCell<TreeNode>>>> = items.iter()
+                .map(|v| {
+                    match v.as_ref() {
+                        Some(value) => TreeNodeAdditions::new(*value),
+                        None => None
+                    }
+                })
+                .collect();
+
+            if items.is_empty() { None }
+            else {
+                let mut queue = VecDeque::new();
+                let mut is_left = true;
+                let mut current: Option<Rc<RefCell<TreeNode>>> = None;
+                let result = items[0].clone();
+
+                queue.push_back(result.clone());
+
+                for index in 1..items.len() {
+                    if is_left {
+                        current = queue.pop_front().unwrap(); // within length of items
+                        let left = &items[index];
+                        current.set_left(left.clone());
+                        if left.is_some() {
+                            queue.push_back(left.clone());
+                        }
+                    } else {
+                        let right = &items[index];
+                        current.set_right(right.clone());
+                        if right.is_some() {
+                            queue.push_back(right.clone());
+                        }
+                    }
+                    is_left = !is_left;
+                }
+
+                result
+            }
+        }
+
     }
 
 }
 
-// import scala.collection.mutable.ArrayBuffer
-// import scala.collection.mutable.Queue
-//
-// class Codec {
-//   def serialize(root: TreeNode): String = {
-//     if (root == null) "[]"
-//     else {
-//       val queue: Queue[TreeNode] = new Queue[TreeNode]()
-//       queue.enqueue(root)
-//       val ab: ArrayBuffer[TreeNode] = new ArrayBuffer[TreeNode]()
-//       serialize(queue, ab)
-//       ab.map(item => if (item == null) "null" else item.value.toString()).mkString("[", ",",
-//       "]")
-//     }
-//  }
-//
-//  def serialize (queue.nonEmpty) {
-//    while (queue.nonEmpty) {
-//      val node = queue.dequeue()
-//      ab.addOne(node)
-//      if (node != null) {
-//        queue.enqueue(node.left)
-//        queue.enqueue(node.right)
-//      }
-//    }
-//  }
-//
-//  def deserialize(data: String): TreeNode = {
-//    if (data == "[]") null
-//    else {
-//      val items: Array[String] = data.stripPrefix("[").stripSuffix("]").split(",")
-//      val nodes: Array[TreeNode] = items.map {
-//        value => if (value == "null") null else new TreeNode(value.toInt)
-//      }
-//      val queue: Queue[TreeNode] = new Queue[TreeNode]
-//      queue.enqueue(nodes.head)
-//
-//      var index = 1
-//      var isLeft: Boolean = true
-//      var node: TreeNode = null
-//      while (index < nodes.size) {
-//        if (isLeft) {
-//          node = queue.dequeue();
-//          node.left = nodes(index)
-//          if (node.left != null) queue.enqueue(node.left)
-//        } else {
-//          node.right = nodes(index)
-//          if (node.right != null) queue.enqueue(node.right)
-//        }
-//        isLeft = !isLeft
-//        index += 1
-//      }
-//      nodes.head
-//    }
-//  }
-//}
+#[cfg(test)]
+mod tests {
+    use crate::tree_node_additions::TreeNodeAdditions;
+    use super::Codec;
+
+    #[test]
+    fn serialize_none() {
+        let codec = Codec::new();
+        let result = codec.serialize(None);
+        assert_eq!(result, "[]");
+    }
+
+    #[test]
+    fn serialize_single_node() {
+        let codec = Codec::new();
+        let one = TreeNodeAdditions::new(1);
+        let result = codec.serialize(one);
+        assert_eq!(result, "[1]");
+    }
+
+    #[test]
+    fn deserialize_brackets() {
+        let codec = Codec::new();
+        let result = codec.deserialize("[]".to_string());
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn deserialize_single_node() {
+        let codec = Codec::new();
+        let result = codec.deserialize("[1]".to_string());
+        let one = TreeNodeAdditions::new(1);
+        assert_eq!(result, one);
+    }
+
+    #[test]
+    fn deserialize_three_node() {
+        let codec = Codec::new();
+        let result = codec.deserialize("[1,2,3]".to_string());
+        let three = TreeNodeAdditions::new(3);
+        let two = TreeNodeAdditions::new(2);
+        let one = TreeNodeAdditions::with_children(1, two, three);
+        assert_eq!(result, one);
+    }
+
+}
+
