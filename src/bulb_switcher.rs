a3f4d17737a70a445a181322c7dda7b64898fad2
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-27 16:00:20 -0700

    Added Description for Bulb Switcher

--- src/bulb_switcher.rs
+++ src/bulb_switcher.rs
@@ -1,6 +1,12 @@
-struct Solution {
-
-}
+/// There are `n` bulbs that are initially off. You first turn on all the
+/// bulbs, then you turn off every second bulb.
+///
+/// On the third round, you toggle every third bulb (turning on if it's off or
+/// turning off if it's on). For the `ith` round, you toggle every `i` bulb.
+/// For the `nth` round, you only toggle the last bulb.
+///
+/// Return the number of bulbs that are on after `n` rounds.
+struct Solution;
 
 impl Solution {
 
@@ -13,7 +19,6 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::Solution;
-
 
     #[test]
     fn example_1() {
@@ -64,47 +69,4 @@ mod tests {
         assert_eq!(result, 9999);
     }
 
-    // 0 -> 0
-    // 1 -> 1
-    // 2 -> 1
-    // 3 -> 1
-    // 4 -> 2
-    // 5 -> 2
-    // 6 -> 2
-    // 7 -> 2
-    // 8 -> 2
-    // 9 -> 3
-    // 10 -> 3
-    // 11 -> 3
-    // 12 -> 3
-    // 13 -> 3
-    // 14 -> 3
-    // 15 -> 3
-    // 16 -> 4
-    // 17 -> 4
-    // 18 -> 4
-    // 19 -> 4
-    // 20 -> 4
-    // 21 -> 4
-    // 22 -> 4
-    // 23 -> 4
-    // 24 -> 4
-    // 25 -> 5
-
-
-
 }
-
-// X Y Y X Y Y
-
-// 10
-// 1 2 3 4 5 6 7 8 9 10 (1) (10)
-// 1 X 3 X 5 X 7 X 9 X  (2) (5)
-// 1 X X X 5 6 7 X X X  (3) (4)
-// 1 X X 4 5 6 7 8 X X  (4) (6)
-// 1 X X 4 X 6 7 8 X 0  (5) (6)
-// 1 X X 4 X X 7 8 X 0  (6) (5)
-// 1 X X 4 X X X 8 X 0  (7) (4)
-// 1 X X 4 X X X X X 0  (8) (3)
-// 1 X X 4 X X X X 9 0  (9) (4)
-// 1 X X 4 X X X X 9 X  (10) (3)
