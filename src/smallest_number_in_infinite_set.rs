76c274375bb05ba5467323059e6dfde14323f266
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-28 09:39:00 -0700

    Added Missing Description to Smallest Number in Infinite Set

--- src/smallest_number_in_infinite_set.rs
+++ src/smallest_number_in_infinite_set.rs
@@ -2,6 +2,15 @@ use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::collections::HashSet;
 
+/// You have a set which contains all positive integers `[1, 2, 3, 4, 5, ...]`.
+///
+/// Implement the `SmallestInfiniteSet` class:
+/// * `SmallestInfiniteSet()` Initializes the SmallestInfiniteSet object to
+///   contain all positive integers.
+/// * `int popSmallest()` Removes and returns the smallest integer contained
+///   in the infinite set.
+/// * `void addBack(int num)` Adds a positive integer `num` back into the
+///   infinite set, if it is not already in the infinite set.
 struct SmallestInfiniteSet {
     items: HashSet<i32>,
     heap: BinaryHeap<Reverse<i32>>
