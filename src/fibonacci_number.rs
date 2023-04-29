1099819a16138be02162e3156fbb6bcaa95640f9
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-28 20:10:23 -0700

    Removed Triple Backtick from Description (Seen as Test)

--- src/fibonacci_number.rs
+++ src/fibonacci_number.rs
@@ -2,10 +2,10 @@
 /// the Fibonacci sequence, such that each number is the sum of the two
 /// preceding ones, starting from `0` and `1`. That is
 ///
-/// ```
+/// `
 /// F(0) = 0, F(1) = 1
 /// F(n) = F(n-1) + F(n-2), for n > 1.
-/// ```
+/// `
 ///
 /// Given `n`, calculate `F(n)`.
 struct Solution;
