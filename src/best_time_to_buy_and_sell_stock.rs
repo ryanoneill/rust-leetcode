9f53567db1d5c11ab83fec2e97aae42f30110bb7
Ryan O'Neill <ryanoneill@gmail.com>
2023-04-28 21:11:55 -0700

    Added Best Time to Buy and Sell Stock Structure

---
+++ src/best_time_to_buy_and_sell_stock.rs
@@ -0,0 +1,18 @@
+/// You are given an array `prices` where `prices[i]` is the price of a given
+/// stock on the `ith` day.
+///
+/// You want to maximize your profit by choosing a single day to buy one stock
+/// and choosing a different day in the future to sell that stock.
+///
+/// Return the maximum profit you can achieve from this transaction. If you
+/// cannot achieve any profit, return `0`.
+struct Solution;
+
+impl Solution {
+
+    // TODO: Implement
+    pub fn max_profit(_prices: Vec<i32>) -> i32 {
+        0
+    }
+
+}
