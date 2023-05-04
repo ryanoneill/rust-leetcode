c61cde6a7dd0cf8bca6052c3ba3773b4ef0071ff
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-04 13:47:25 -0700

    Added Missing Description for Find K Closest Elements

--- src/find_k_closest_elements.rs
+++ src/find_k_closest_elements.rs
@@ -16,6 +16,13 @@ impl State {
 
 }
 
+/// Given a sorted integer array `arr`, two integers `k` and `x`, return the
+/// `k` closest integers to `x` in the array. The result should also be sorted
+/// in ascending order.
+///
+/// An integer `a` is closer to `x` than an integer `b` if:
+/// * `|a - x| < |b - x|`, or
+/// * `|a - x| == |b - x|` and `a < b`
 struct Solution;
 
 impl Solution {
