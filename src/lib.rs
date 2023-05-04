2060b3a8189c07fe0bc1337b2d44fc9f5c30d838
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-03 20:17:26 -0700

    Added Meeting Rooms

--- src/lib.rs
+++ src/lib.rs
@@ -84,6 +84,8 @@ pub mod lowest_common_ancestor_of_a_binary_tree; // 236
 pub mod sliding_window_maximum; // 239
 pub mod valid_anagram; // 240
 
+pub mod meeting_rooms; // 252
+
 pub mod add_digits; // 258
 
 pub mod closest_binary_search_tree_value; // 270
@@ -109,6 +111,8 @@ pub mod moving_average_from_data_stream; // 346
 pub mod top_k_frequent_elements; // 347
 
 pub mod intersection_of_two_arrays_ii; // 350
+
+pub mod kth_smallest_element_in_a_sorted_matrix; // 378
 
 pub mod ransom_note; // 383
 
---
+++ src/meeting_rooms.rs
@@ -0,0 +1,69 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+#[derive(Copy, Clone, PartialOrd, PartialEq, Eq, Ord, Hash)]
+struct Meeting {
+    start: i32,
+    end: i32,
+}
+
+impl Meeting {
+
+    fn new(start: i32, end: i32) -> Self {
+        Self { start, end }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+
+    pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> bool {
+        let mut min_heap = BinaryHeap::new();
+        for interval in intervals {
+            min_heap.push(Reverse(Meeting::new(interval[0], interval[1])));
+        }
+
+        let mut attending = None;
+        let mut result = true;
+
+        while !min_heap.is_empty() {
+            let next = min_heap.pop().unwrap().0;
+            if attending.is_none() {
+                attending = Some(next);
+            } else {
+                let current = attending.unwrap();
+
+                if next.start >= current.end {
+                    attending = Some(next);
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+        }
+
+        result
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn example_1() {
+        let intervals = vec![vec![0,30], vec![5,10], vec![15,20]];
+        let result = Solution::can_attend_meetings(intervals);
+        assert!(!result);
+    }
+
+    #[test]
+    fn example_2() {
+        let intervals = vec![vec![7,10], vec![2,4]];
+        let result = Solution::can_attend_meetings(intervals);
+        assert!(result);
+    }
+
+}
