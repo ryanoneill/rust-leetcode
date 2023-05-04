8f4294f0e55f770d4c615daee1d3bd0a6a059d85
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-04 14:02:59 -0700

    Rust Format Botches Lib Ordering by Problem Number

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,9 @@ pub mod container_with_most_water; // 11
 
 pub mod longest_common_prefix; // 14
 
-pub mod merge_two_sorted_lists;
 pub mod remove_nth_node_from_end_of_list; // 19
-pub mod valid_parentheses; // 20 // 21
+pub mod valid_parentheses; // 20
+pub mod merge_two_sorted_lists; // 21
 
 pub mod swap_nodes_in_pairs; // 24
 
@@ -47,8 +47,8 @@ pub mod simplify_path; // 71
 
 pub mod remove_duplicates_from_sorted_array_ii; // 80
 
-pub mod remove_duplicates_from_sorted_list;
-pub mod remove_duplicates_from_sorted_list_ii; // 82 // 83
+pub mod remove_duplicates_from_sorted_list_ii; // 82
+pub mod remove_duplicates_from_sorted_list; // 83
 
 pub mod reverse_linked_list_ii; // 92
 
@@ -130,8 +130,8 @@ pub mod find_largest_value_in_each_tree_row; // 515
 
 pub mod minimum_absolute_difference_in_bst; // 530
 
-pub mod diameter_of_binary_tree;
-pub mod zero_one_matrix; // 542 // 543
+pub mod zero_one_matrix; // 542
+pub mod diameter_of_binary_tree; // 543
 
 pub mod number_of_provinces; // 547
 
