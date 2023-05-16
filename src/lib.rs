c3ea14d8edd079365848bded8c5795bb91fca1df
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-15 21:23:18 -0700

    Added Search a 2D Matrix

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,7 @@ pub mod plus_one; // 66
 pub mod climbing_stairs; // 70
 pub mod simplify_path; // 71
 
+pub mod search_a_2d_matrix; // 74 ✓
 pub mod sort_colors; // 75
 
 pub mod remove_duplicates_from_sorted_array_ii; // 80
