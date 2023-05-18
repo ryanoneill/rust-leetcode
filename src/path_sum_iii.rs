8073fd48ada98474bac3b975989b6a631b6bf137
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-18 08:46:32 -0700

    Added Path Sum III

--- src/path_sum_iii.rs
+++ src/path_sum_iii.rs
@@ -12,9 +12,38 @@ struct Solution;
 
 impl Solution {
 
-    // TODO: Implement
-    pub fn path_sum(_root: Option<Rc<RefCell<TreeNode>>>, _target_sum: i32) -> i32 {
-        0
+    fn worker(root: &Option<Rc<RefCell<TreeNode>>>, target_sum: i32, path: Vec<i64>) -> i32 {
+        match root {
+            Some(rc) => {
+                let mut path = path;
+                let node = rc.borrow();
+                let value = node.val;
+                let mut result = 0;
+
+                path = path.iter_mut().map(|v| *v + value as i64).collect();
+                path.push(value as i64);
+                for sum in &path {
+                    if *sum == target_sum as i64 {
+                        result += 1;
+                    }
+                }
+
+                if node.left.is_some() {
+                    result += Self::worker(&node.left, target_sum, path.clone());
+                }
+                if node.right.is_some() {
+                    result += Self::worker(&node.right, target_sum, path.clone());
+                }
+                result
+            }
+            None => {
+                0
+            }
+        }
+    }
+
+    pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> i32 {
+        Self::worker(&root, target_sum, Vec::new())
     }
 
 }
@@ -24,7 +53,6 @@ mod tests {
     use crate::serialize_and_deserialize_binary_tree::Codec;
     use super::Solution;
 
-    #[ignore]
     #[test]
     fn example_1() {
         let data = "[10,5,-3,3,2,null,11,3,-2,null,1]".to_string();
@@ -34,7 +62,6 @@ mod tests {
         assert_eq!(result, 3);
     }
 
-    #[ignore]
     #[test]
     fn example_2() {
         let data = "[5,4,8,11,null,13,4,7,2,null,null,5,1]".to_string();
@@ -44,10 +71,18 @@ mod tests {
         assert_eq!(result, 3);
     }
 
-    #[ignore]
     #[test]
     fn empty() {
         let data = "[]".to_string();
+        let codec = Codec::new();
+        let root = codec.deserialize(data);
+        let result = Solution::path_sum(root, 0);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn large_numbers() {
+        let data = "[1000000000,1000000000,null,294967296,null,1000000000,null,1000000000,null,1000000000]".to_string();
         let codec = Codec::new();
         let root = codec.deserialize(data);
         let result = Solution::path_sum(root, 0);
