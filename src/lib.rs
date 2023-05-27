d7adbcb1df11b1f13a3ef4d3bf65925662133cc6
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-26 18:59:47 -0700

    Added Longest Palindromic Substring

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub mod two_sum; // 1 ✓
 pub mod add_two_numbers; // 2
 pub mod length_of_longest_substring; // 3 ✓
 pub mod median_of_two_sorted_arrays; // 4
-pub mod longest_palindromic_substring; // 5
+pub mod longest_palindromic_substring; // 5 ✓
 pub mod zigzag_conversion; // 6 ✓
 pub mod reverse_integer; // 7 ✓
 pub mod string_to_integer; // 8 ✓
--- src/longest_palindromic_substring.rs
+++ src/longest_palindromic_substring.rs
@@ -2,29 +2,110 @@
 pub struct Solution;
 
 impl Solution {
-    // TODO: Implement
-    pub fn longest_palindrome(_s: String) -> String {
-        // let letters: Vec<char> = s.chars().collect();
 
-        "".to_string()
+    fn expand_out_from(letters: &Vec<char>, left: usize, right: usize) -> usize {
+        let n = letters.len();
+        let mut result = 0;
+
+        let mut left = left;
+        let mut right = right;
+
+        loop {
+            let left_letter = letters[left];
+            let right_letter = letters[right];
+            if left_letter == right_letter {
+                if left == right { 
+                    result += 1; 
+                } else {
+                    result += 2;
+                }
+
+                if left == 0 {
+                    break;
+                } else if right == n - 1 {
+                    break;
+                } else {
+                    left -= 1;
+                    right += 1;
+                }
+            } else {
+                break;
+            }
+        }
+
+        result
     }
+
+    fn get_palindrome(letters: &Vec<char>, i: usize, len: usize) -> String {
+        let start = i - (len - 1) / 2;
+        let end = i + len / 2 + 1;
+        letters[start..end].iter().collect()
+    }
+
+    pub fn longest_palindrome(s: String) -> String {
+        let mut result = String::from("");
+        let mut result_len = 0;
+
+        let letters: Vec<char> = s.chars().collect();
+        let n = letters.len();
+
+        for i in 0..n {
+            let centered = Self::expand_out_from(&letters, i, i);
+            if centered >= result_len {
+                result = Self::get_palindrome(&letters, i, centered);
+                result_len = result.len();
+            }
+            if i < n-1 {
+                let between = Self::expand_out_from(&letters, i, i+1);
+                if between >= result_len {
+                    result = Self::get_palindrome(&letters, i, between);
+                    result_len = result.len();
+                }
+            }
+        }
+
+        result
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::Solution;
 
-    #[ignore]
     #[test]
     fn example_1() {
-        let result = Solution::longest_palindrome("babad".to_string());
-        assert_eq!(result, "bab");
+        let s = "babad".to_string();
+        let result = Solution::longest_palindrome(s);
+        assert!(result == "bab" || result == "aba")
     }
 
-    #[ignore]
     #[test]
     fn example_2() {
-        let result = Solution::longest_palindrome("cbbd".to_string());
+        let s = "cbbd".to_string();
+        let result = Solution::longest_palindrome(s);
         assert_eq!(result, "bb");
     }
+
+    #[test]
+    fn longer() {
+        let s = "hellohowareyoutodayracecarblueseriously".to_string();
+        let result = Solution::longest_palindrome(s);
+        assert_eq!(result, "racecar");
+    }
+
+    #[test]
+    fn single() {
+        let s = "a".to_string();
+        let result = Solution::longest_palindrome(s);
+        assert_eq!(result, "a");
+    }
+
+    #[test]
+    fn double() {
+        let s = "bb".to_string();
+        let result = Solution::longest_palindrome(s);
+        assert_eq!(result, "bb");
+    }
+
 }
