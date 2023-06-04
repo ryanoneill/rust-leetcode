ef9b3dff13b40fb759744f2ea8c391a81492b6f6
Ryan O'Neill <ryanoneill@gmail.com>
2023-06-04 10:39:38 -0700

    Added Minimum Cost to Make All Characters Equal Structure

--- src/lib.rs
+++ src/lib.rs
@@ -481,3 +481,4 @@ pub mod maximum_subsequence_score; // 2542 ✓
 
 pub mod remove_trailing_zeros_from_a_string; // 2710 ✓
 pub mod difference_of_number_of_distinct_values_on_diagonals; // 2711 ✓
+pub mod minimum_cost_to_make_all_characters_equal; // 2712
---
+++ src/minimum_cost_to_make_all_characters_equal.rs
@@ -0,0 +1,44 @@
+/// You are given a 0-indexed binary string `s` of length `n` on which you can
+/// apply two types of operations:
+///
+/// * Choose an index `i` and invert all characters from index `0` to `i` (both
+///   inclusive), with a cost of `i + 1`
+///
+/// * Choose an index `i` and invert all characters from index `i` to index
+///   `n - 1` (both inclusive), with a cost of `n - i`
+///
+/// Return the minimum cost to make all characters of the string equal.
+///
+/// Invert a character means if its values is '0' it becomes '1' and vice-versa.
+struct Solution;
+
+impl Solution {
+
+    // TODO: Implement
+    pub fn minimum_cost(s: String) -> i64 {
+        0
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[ignore]
+    #[test]
+    fn example_1() {
+        let s = "0011".to_string();
+        let result = Solution::minimum_cost(s);
+        assert_eq!(result, 2);
+    }
+
+    #[ignore]
+    #[test]
+    fn example_2() {
+        let s = "010101".to_string();
+        let result = Solution::minimum_cost(s);
+        assert_eq!(result, 9);
+    }
+
+}
