b1e011879f5eb798c689ae116f95e4e472a2aa4b
Ryan O'Neill <ryanoneill@gmail.com>
2023-06-19 12:05:16 -0700

    Marked Sort Colors as Submitted

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ pub mod simplify_path; // 71
 
 pub mod set_matrix_zeroes; // 73 ✓
 pub mod search_a_2d_matrix; // 74 ✓
-pub mod sort_colors; // 75
+pub mod sort_colors; // 75 ✓
 pub mod minimum_window_substring; // 76 ✓
 pub mod combinations; // 77 ✓
 pub mod subsets; // 78 ✓
