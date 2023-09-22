0d926ff770975a229434523104178b867f76ac5b
Ryan O'Neill <ryanoneill@gmail.com>
2023-09-22 13:27:55 -0700

    Added Maximum Product Subarray

--- src/lib.rs
+++ src/lib.rs
@@ -145,7 +145,7 @@ pub mod sort_list; // 148
 
 pub mod evaluate_reverse_polish_notation; // 150 ✓
 pub mod reverse_words_in_a_string; // 151 ✓
-pub mod maximum_product_subarray; // 152
+pub mod maximum_product_subarray; // 152 ✓
 pub mod find_minimum_in_rotated_sorted_array; // 153 ✓
 
 pub mod min_stack; // 155
--- src/maximum_product_subarray.rs
+++ src/maximum_product_subarray.rs
@@ -6,8 +6,50 @@ struct Solution;
 
 impl Solution {
 
-    pub fn max_product(_nums: Vec<i32>) -> i32 {
-        0
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut result = i32::MIN;
+
+        let mut negative_result = i32::MAX;
+        let mut positive_result = i32::MIN;
+
+        for num in nums {
+            if num == 0 {
+                result = result.max(0);
+                negative_result = i32::MAX;
+                positive_result = i32::MIN;
+            } else if num > 0 {
+                if negative_result != i32::MAX {
+                    negative_result *= num;
+                }
+                if positive_result == i32::MIN {
+                    positive_result = num;
+                } else {
+                    positive_result *= num;
+                }
+                result = result.max(positive_result);
+            } else { // num < 0
+                if negative_result == i32::MAX {
+                    if positive_result != i32::MIN {
+                        negative_result = positive_result * num;
+                        positive_result = i32::MIN;
+                    } else {
+                        negative_result = num;
+                    }
+                    result = result.max(num);
+                } else {
+                    let temp = negative_result * num;
+                    negative_result = if positive_result != i32::MIN {
+                        num * positive_result
+                    } else {
+                        num
+                    };
+                    positive_result = temp;
+                    result = result.max(positive_result);
+                }
+            }
+        }
+
+        result
     }
 
 }
@@ -16,7 +58,6 @@ impl Solution {
 mod tests {
     use super::Solution;
 
-    #[ignore]
     #[test]
     fn example_1() {
         let nums = vec![2,3,-2,4];
@@ -24,7 +65,32 @@ mod tests {
         assert_eq!(result, 6);
     }
 
+    #[test]
+    fn example_2() {
+        let nums = vec![-2,0,-1];
+        let result = Solution::max_product(nums);
+        assert_eq!(result, 0);
+    }
 
-    // TODO: Add More Tests
+    #[test]
+    fn example_3() {
+        let nums = vec![-2];
+        let result = Solution::max_product(nums);
+        assert_eq!(result, -2);
+    }
+
+    #[test]
+    fn example_4() {
+        let nums = vec![7,-2,-4];
+        let result = Solution::max_product(nums);
+        assert_eq!(result, 56);
+    }
+
+    #[test]
+    fn example_5() {
+        let nums = vec![-1,-2,-9,-6];
+        let result = Solution::max_product(nums);
+        assert_eq!(result, 108);
+    }
 
 }
