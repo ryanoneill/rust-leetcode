d1294e45432b3ea92318ec554d31b6116ed939e7
Ryan O'Neill <ryanoneill@gmail.com>
2024-12-31 17:41:02 -0800

    Update Squares of a Sorted Array
    This commit modifies `Squares of a Sorted Array` to no longer
    create intermediate `Vec`s and instead work based off of `Iterator`s
    into the original `Vec`. Submission time dropped from 11ms to 0ms.

--- src/squares_of_a_sorted_array.rs
+++ src/squares_of_a_sorted_array.rs
@@ -2,32 +2,29 @@ struct Solution;
 
 impl Solution {
 
-    fn merge(a: &Vec<i32>, b: &Vec<i32>) -> Vec<i32> {
-        let mut a_iter = a.into_iter();
-        let mut b_iter = b.into_iter();
-
+    fn merge(a_iter: &mut dyn Iterator<Item=i32>, b_iter: &mut dyn Iterator<Item=i32>, n: usize) -> Vec<i32> {
         let mut a_value = a_iter.next();
         let mut b_value = b_iter.next();
 
-        let mut result = Vec::with_capacity(a.len() + b.len());
+        let mut result = Vec::with_capacity(n);
 
         while a_value.is_some() || b_value.is_some() {
             match (a_value, b_value) {
                 (Some(a), Some(b)) => {
                     if a <= b {
-                        result.push(*a);
+                        result.push(a);
                         a_value = a_iter.next();
                     } else {
-                        result.push(*b);
+                        result.push(b);
                         b_value = b_iter.next();
                     }
                 }
                 (Some(a), None) => {
-                    result.push(*a);
+                    result.push(a);
                     a_value = a_iter.next();
                 }
                 (None, Some(b)) => {
-                    result.push(*b);
+                    result.push(b);
                     b_value = b_iter.next();
                 }
                 _ => { }
@@ -39,10 +36,11 @@ impl Solution {
 
     pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
         let i = nums.partition_point(|&num| num < 0);
-        let (below, above) = nums.split_at(i);
-        let below_squared = below.into_iter().map(|x| x * x).rev().collect();
-        let above_squared = above.into_iter().map(|x| x * x).collect();
-        Self::merge(&below_squared, &above_squared)
+        let (left, right) = nums.split_at(i);
+        let mut lefts = left.iter().copied().map(|x| x * x).rev();
+        let mut rights = right.iter().copied().map(|x| x * x);
+        let result = Self::merge(&mut lefts, &mut rights, nums.len());
+        result
     }
 
 }
